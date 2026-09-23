fe5f0d17447b2c0845c2491c8394b9be887fe303
ewer quintero flores <ewerfadullflorez@gmail.com>
2026-09-23 17:12:15 -0500

    Primera conexion JDBC con SQLite

--- src/biblioteca/Main.java
+++ src/biblioteca/Main.java
@@ -1,4 +1,16 @@
 package biblioteca;
 
+import biblioteca.db.ConexionDB;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) {
+        try (Connection con = ConexionDB.conectar()) {
+            System.out.println("Conectado");
+        } catch (SQLException e) {
+            System.out.println("No fue posible la conexion");
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/biblioteca/db/ConexionDB.java
+++ src/biblioteca/db/ConexionDB.java
@@ -1,4 +1,13 @@
 package biblioteca.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionDB {
+    private static final String URL  = "jdbc:sqlite:data/libros2.db";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
 }
